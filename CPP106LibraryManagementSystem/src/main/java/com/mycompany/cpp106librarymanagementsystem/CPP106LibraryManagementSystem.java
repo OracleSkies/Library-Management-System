d993772f62777c3acf2c7cf6d771ed2c20e7f97f
OracleSkies <153785033+OracleSkies@users.noreply.github.com>
2024-11-13 17:05:48 +0800

    Project File Creation

---
+++ CPP106LibraryManagementSystem/src/main/java/com/mycompany/cpp106librarymanagementsystem/CPP106LibraryManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cpp106librarymanagementsystem;
+
+/**
+ *
+ * @author Jayjay
+ */
+public class CPP106LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
